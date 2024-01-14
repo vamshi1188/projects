22ad00faacda89a37f5c1bcb1432f4c393e5d035
vamshi1188 <saivamshik11@gmail.com>
2024-01-14 20:52:02 +0530

    deleted text.go file and made changes in booking app

--- Booking-app/main.go
+++ Booking-app/main.go
@@ -3,6 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	var conferenceName = " go conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("we have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	//ask user to enter their name
+
+	userName = "tom"
+	userTickets = 2
+	fmt.Println(userName)
+	fmt.Printf("user %v booked %v tickets\n", userName, userTickets)
 
 }
