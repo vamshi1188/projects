4782850f8d1b272180f5deaa62b1a2e8dadbe859
vamshi1188 <saivamshik11@gmail.com>
2024-01-27 16:09:38 +0530

    sg

--- Booking-app/main.go
+++ Booking-app/main.go
@@ -6,31 +6,38 @@ func main() {
 	conferenceName := " go conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = 50
+	bookings := []string{}
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("we have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	//ask the  user to enter their name
-	fmt.Print("Enter ur first name: ")
-	fmt.Scan(&firstName)
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		//ask the  user to enter their name
+		fmt.Print("Enter ur first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Print("Enter ur last name: ")
-	fmt.Scan(&lastName)
+		fmt.Print("Enter ur last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Print("Enter ur email address: ")
-	fmt.Scan(&email)
+		fmt.Print("Enter ur email address: ")
+		fmt.Scan(&email)
 
-	fmt.Print("Enter number of tickets: ")
-	fmt.Scan(&userTickets)
+		fmt.Print("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("thank you %v %v for booking %v tickets. You will receive confermation mail at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
+		fmt.Printf("thank you %v %v for booking %v tickets. You will receive confermation mail at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
+
+		fmt.Printf("these are all our bookings: %v\n", bookings)
+
+	}
 
 }
