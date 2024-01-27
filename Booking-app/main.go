7e98699476bdc82d6ce6acc023287092336431ef
vamshi1188 <saivamshik11@gmail.com>
2024-01-27 15:27:05 +0530

    hbd

--- Booking-app/main.go
+++ Booking-app/main.go
@@ -5,19 +5,32 @@ import "fmt"
 func main() {
 	conferenceName := " go conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("we have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("get your tickets here to attend")
 
-	var userName string
-	var userTickets int
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 	//ask the  user to enter their name
-	fmt.Println("Enter ur first name")
-	fmt.Scan(&userName)
+	fmt.Print("Enter ur first name: ")
+	fmt.Scan(&firstName)
 
-	userTickets = 2
-	fmt.Printf("user %v booked %v tickets\n", userName, userTickets)
+	fmt.Print("Enter ur last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Print("Enter ur email address: ")
+	fmt.Scan(&email)
+
+	fmt.Print("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("thank you %v %v for booking %v tickets. You will receive confermation mail at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
 
 }
