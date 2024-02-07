1d4344ac21cdd2df85e6da7c465018ff64bad1ac
vamshi1188 <saivamshik11@gmail.com>
2024-02-07 20:36:46 +0530

    svm

--- Booking-app/main.go
+++ Booking-app/main.go
@@ -52,7 +52,7 @@ func main() {
 
 func greetUSers() {
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("we have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Printf("we have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("get your tickets here to attend")
 }
 
